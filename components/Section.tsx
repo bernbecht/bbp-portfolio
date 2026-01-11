31f29af2d4cdbe8ef35b693d6637e5cbca5c49cd
Bernardo Bechtold <bechtold.bernardo@gmail.com>
2026-01-11 16:35:20 -0300

    ✨ creates Section component

---
+++ components/Section.tsx
@@ -0,0 +1,33 @@
+type SectionProps = {
+  id: string
+  label: string
+  title?: string
+  children: React.ReactNode
+}
+
+export function Section({id, label, title, children}: SectionProps) {
+  return (
+    <section
+      id={id}
+      aria-labelledby={title ? `${id}-title` : undefined} // @TODO: add smart default
+      className="scroll-mt-24 py-24 grid grid-cols-[1fr_900px_1fr]"
+    >
+        <span className="block text-xs font-mono text-gray-500">
+          {label}
+        </span>
+      <div className="w-900px px-4">
+
+        {title && (
+          <h2
+            id={`${id}-title`}
+            className="mb-8 text-2xl font-semibold tracking-tight"
+          >
+            {title}
+          </h2>
+        )}
+
+        {children}
+      </div>
+    </section>
+  );
+}
