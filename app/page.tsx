97a046846f8f76b6cf0a4b853be5fa795e269038
Bernardo Bechtold <bernardo.bechtold@shopify.com>
2026-01-12 13:41:00 -0300

    ✨ adds Section Paragraph component

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,15 @@
-import { Section } from "@/components/Section";
-import { Literata } from "next/font/google";
-
-const literata = Literata({
-  subsets: ["latin"],
-});
+import { Section, SectionParagraph } from "@/components/Section";
 
 export default function Home() {
   return (
     <>
       <Section id="what" label="/what">
-        <p className={literata.className + " text-2xl text-gray-700"}>
+        <SectionParagraph>
           Mussum Ipsum, cacilds vidis litro abertis. Diuretics paradis num copo
           é motivis de denguis. Sapien in monti palavris qui num significa nadis
           i pareci latim. Todo mundo vê os porris que eu tomo, mas ninguém vê os
           tombis que eu levo! Bota 1 metro de cachacis aí pra viagem!
-        </p>
+        </SectionParagraph>
       </Section>
     </>
   );
