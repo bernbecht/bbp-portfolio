62dc628fdaf6a48d3fe2b2a7355c752213b33a3a
Bernardo Bechtold <bechtold.bernardo@gmail.com>
2026-01-15 14:23:32 -0300

    💄 adds dither effect to the CompanyGrid

--- components/CompanyGrid.tsx
+++ components/CompanyGrid.tsx
@@ -9,32 +9,37 @@ type Company = {
 };
 
 const companies: Company[] = [
-  { name: "Axonify", logo: AxonifyLogo },
-  { name: "CIT", logo: CitLogo },
-  { name: "SAP", logo: SapLogo },
   { name: "Shopify", logo: ShopifyLogo },
+  { name: "SAP", logo: SapLogo },
+  { name: "CIT", logo: CitLogo },
+  { name: "Axonify", logo: AxonifyLogo },
 ];
 
 export function CompaniesGrid() {
   return (
-    <ul className="grid grid-cols-2 border border-solid border-gray-950">
-      {companies.map((company, index) => {
-        const isOdd = index % 2 === 1;
-
-        return (
-          <li
-            key={company.name}
-            className={`
-              border-b border-b-gray-300
+    <>
+      <ul className="grid grid-cols-2 border border-solid border-gray-950">
+        {companies.map((company, index) => {
+          const isOdd = index % 2 === 1;
+          const isLastRow = index > 1;
+          return (
+            <li
+              key={company.name}
+              className={`
+              ${!isLastRow ? "border-b border-b-gray-300" : ""}
               ${!isOdd ? "border-r border-r-gray-300 " : ""}
             `}
-          >
-            <a href="#" className="flex items-center justify-center">
-              <company.logo className="w-32 h-16" />
-            </a>
-          </li>
-        );
-      })}
-    </ul>
+            >
+              <a href="#" className="flex items-center justify-center">
+                <div className="py-8">
+                  <company.logo className="w-32 h-16" />
+                </div>
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+      <div className="dither w-full h-3"></div>
+    </>
   );
 }
