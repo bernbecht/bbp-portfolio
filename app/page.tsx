68cb06c0ec9f35f9acadd74734711c4a242369f7
Bernardo Bechtold <bernardo.bechtold@shopify.com>
2026-01-22 10:15:53 -0300

    ✨ adds my tools sections

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,12 @@ import { Hero } from "@/components/Hero";
 import { Section, SectionParagraph } from "@/components/Section";
 import { StatusBar } from "@/components/StatusBar";
 
+import { Literata } from "next/font/google";
+
+const literata = Literata({
+  subsets: ["latin"],
+});
+
 export default function Home() {
   return (
     <>
@@ -22,6 +28,26 @@ export default function Home() {
           Atuei em empresas globais, dentro de times diversos e multiculturais.
         </SectionParagraph>
         <CompaniesGrid />
+      </Section>
+      <Section id="how" label="/how">
+        <SectionParagraph>
+          Pesquisa de usuário, criação e implementação de jornadas de usuário,
+          criação e desenvolvimento de design systems, melhoramento da developer
+          experience, aproximando design e engenharia e facilitando workshops e
+          treinamentos de Design Thinking.
+        </SectionParagraph>
+        <h1 className={`tools-section_title mb-8 mt-16 pr-3`}>My tools</h1>
+        <ul className={`tools-grid`}>
+          <li>ReactJS</li>
+          <li>Typescript</li>
+          <li>NodeJS</li>
+          <li>Storybook</li>
+          <li>Playwright</li>
+          <li>Figma</li>
+          <li>MaterialUI</li>
+          <li>ExpressJS</li>
+          <li>PostgreSQL</li>
+        </ul>
       </Section>
       <Footer />
     </>
