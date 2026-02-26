b091bf105aa4e9908cf7703b88bb0d417d507ea3
Bernardo Bechtold <bernardo.bechtold@shopify.com>
2026-02-26 08:22:03 -0300

    💄 adapt StatusBar in the layout

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,43 +1,16 @@
 // import MyIcon from "../public/logo.svg"; // The file path can be outside the public folder
 import Logo from "@assets/logo.svg";
+import { StatusBar } from "./StatusBar";
 
 export default function Header() {
   return (
-    <header className="bg-white/80 backdrop-blur border-b">
+    <header className="bg-white/80 backdrop-blur">
       <nav
         aria-label="Main navigation"
         className="content-container py-4 flex items-center justify-between"
       >
         <Logo />
-        <ul className="flex items-center gap-6">
-          <li>
-            <a
-              href="#about"
-              className="text-m font-semibold text-gray-700 hover:text-black focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
-            >
-              About
-            </a>
-          </li>
-          <li>
-            <a
-              href="#blog"
-              className="text-m font-thin text-gray-700 hover:text-black focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
-            >
-              Blog
-            </a>
-          </li>
-          <li>
-            <a
-              href="#books"
-              className="text-m font-thin text-gray-700 hover:text-black focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
-            >
-              Books
-            </a>
-          </li>
-        </ul>
-        <div>
-          <button>PT</button>/<button>EN</button>
-        </div>
+        <StatusBar />
       </nav>
     </header>
   );
