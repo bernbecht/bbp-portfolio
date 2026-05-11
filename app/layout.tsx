683586b55f125de4add828b8d46647a46ed9a74b
Bernardo Bechtold <bernardo.bechtold@shopify.com>
2026-05-11 20:42:57 -0300

    💄 update body font to use 'font-sans' and remove default font-family

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
-      <body className={`${fontClasses} antialiased`}>
+      <body className={`${fontClasses} font-sans antialiased`}>
         <Header />
         {children}
         <Footer />
