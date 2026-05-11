613c39827cc5b8f39fe54e35dbd6f0bbec574ef8
Bernardo Bechtold <bernardo.bechtold@shopify.com>
2026-05-11 20:38:53 -0300

    🔥 remove dead CSS class in hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export function Hero() {
   return (
     <section className="my-12 md:my-24">
-      <div className="flex items-center gap-4 mb-12 items-end">
+      <div className="flex items-end gap-4 mb-12">
         <Image
           src="/profile.jpeg"
           alt="portrait"
@@ -14,11 +14,11 @@ export function Hero() {
 
         <div className="text-2xl md:text-3xl">
           <p className="text-neutral-500">Glad to see you!</p>
-          <h1 className="hero-name">I am Bernardo</h1>
+          <h1>I am Bernardo</h1>
         </div>
       </div>
 
-      <h2 className="hero-headline text-5xl/14 md:text-7xl/20">
+      <h2 className="text-5xl/14 md:text-7xl/20">
         Front-end Engineer & <br />
         UI/UX Designer <br />
       </h2>
