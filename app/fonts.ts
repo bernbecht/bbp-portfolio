0c53cae5329105a4f3615fb0592443ca1764c685
Bernardo Bechtold <bechtold.bernardo@gmail.com>
2026-05-11 20:55:05 -0300

    🔥 remove Limelight font and associated CSS class to streamline font usage

--- app/fonts.ts
+++ app/fonts.ts
@@ -1,4 +1,4 @@
-import { Geist, Geist_Mono, IBM_Plex_Serif, Limelight } from "next/font/google";
+import { Geist, Geist_Mono, IBM_Plex_Serif } from "next/font/google";
 
 export const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -10,11 +10,6 @@ export const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export const limelight = Limelight({
-  weight: "400",
-  variable: "--font-limelight-400",
-});
-
 export const ibmPlexSerif = IBM_Plex_Serif({
   variable: "--font-ibm-plex-serif-400",
   weight: "400",
@@ -23,6 +18,5 @@ export const ibmPlexSerif = IBM_Plex_Serif({
 export const fontClasses = [
   geistSans.variable,
   geistMono.variable,
-  limelight.variable,
   ibmPlexSerif.variable,
 ].join(" ");
